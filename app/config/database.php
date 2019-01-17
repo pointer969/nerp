b3df32e6c5df831fed0a813d75578ff0d9a47fac
Almeida <MaAlmeida@ad.harman.com>
2019-01-17 15:30:08 -0400

    error log

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 ini_set("allow_url_fopen", 1);
-ini_set("display_errors", 0);
-error_reporting(0);
-ini_set("track_errors","0");
+ini_set("display_errors", 1);
+// error_reporting(0);
+ini_set("track_errors","1");
 
 //* // CONFIGURACAO DE BANCO DE DADOS LOCAL
 define("HOST","rionegrosolucoes.com.br");
